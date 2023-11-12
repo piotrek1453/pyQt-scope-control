accc6adc1c2c3ed1995d8647725a61926b0c8103
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2023-11-12 20:45:47 +0100

    setup script overhaul

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 pip install pyqt5 pyqt5-tools pyvisa pyvisa-py
-python -m virtualenv --system-site-packages venv
+python -m virtualenv venv --system-site-packages
 venv/Scripts/activate
-pip -r requirements.txt
+pip install -I -r requirements.txt
 PAUSE
