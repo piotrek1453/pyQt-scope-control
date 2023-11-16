140c2fb4a3a6cb7f8bb96a409902032bfed0c849
piotrek1453 <piotrek1453@gmail.com>
2023-11-16 20:02:19 +0100

    setup update

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-pip install pyqt5 pyqt5-tools pyvisa pyvisa-py virtualenv
+pip install pyqt5 pyqt5-tools pyvisa pyvisa-py pyvisa_sim virtualenv
 python -m virtualenv venv --system-site-packages
 venv/Scripts/activate
 pip install -I -r requirements.txt
